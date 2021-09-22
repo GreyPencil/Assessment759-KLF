d64a0122a144468d819d1b303cc474650a42cadb
GreyPencil <kangsuze@gmail.com>
2021-09-21 23:32:23 -0400

    finished easy home page

--- react-admin_client/src/pages/home/home.jsx
+++ react-admin_client/src/pages/home/home.jsx
@@ -1,19 +1,10 @@
 import React, {Component} from 'react'
 import {
-  Icon,
-  Card,
-  Statistic,
-  DatePicker,
-  Timeline
+  Carousel,
 } from 'antd'
-import moment from 'moment'
 
-import Line from './line'
-import Bar from './bar'
 import './home.less'
 
-const dateFormat = 'YYYY/MM/DD'
-const {RangePicker} = DatePicker
 
 export default class Home extends Component {
 
@@ -26,75 +17,42 @@ export default class Home extends Component {
   }
 
   render() {
-    const {isVisited} = this.state
+    const contentStyle = {
+      height: '600px',
+      color: '#fff',
+      top: '50%',
+      lineHeight: '60px',
+      textAlign: 'center',
+      fontSize: '30px',
+      verticalAlign: 'middle',
+      display: 'block',
+      wordWrap: 'break-word',
+      background: '#364d79',
+      paddingTop: '200px'
+    };
+
 
     return (
       <div className='home'>
-        <Card
-          className="home-card"
-          title="Product Volume"
-          extra={<Icon style={{color: 'rgba(0,0,0,.45)'}} type="question-circle"/>}
-          style={{width: 250}}
-          headStyle={{color: 'rgba(0,0,0,.45)'}}
-        >
-          <Statistic
-            value={1128163}
-            suffix=""
-            style={{fontWeight: 'bolder'}}
-          />
-          <Statistic
-            value={15}
-            valueStyle={{fontSize: 15}}
-            prefix={'Weekly'}
-            suffix={<div>%<Icon style={{color: 'red', marginLeft: 10}} type="arrow-down"/></div>}
-          />
-          <Statistic
-            value={10}
-            valueStyle={{fontSize: 15}}
-            prefix={'Daily'}
-            suffix={<div>%<Icon style={{color: '#3f8600', marginLeft: 10}} type="arrow-up"/></div>}
-          />
-        </Card>
+        <Carousel autoplay>
+          <div>
+            <div style={contentStyle}>Welcome to Suze's User Management Module </div>
+          </div>
+          <div>
+            <div style={contentStyle}>Here you can create new usernames and passwords, and update user information</div>
+          </div>
+          <div>
+            <div style={contentStyle}>
+              <span>If you have any questions</span>
+              <br/>
+              <span>Don't hesitate to contact me~ </span>
+            </div>
+          </div>
+          <div>
+            <div style={contentStyle}>Hope you like this system!</div>
+          </div>
+        </Carousel>
 
-        <Line/>
-
-        <Card
-          className="home-content"
-          title={<div className="home-menu">
-            <span className={isVisited ? "home-menu-active home-menu-visited" : 'home-menu-visited'}
-                  onClick={this.handleChange(true)}>Views</span>
-            <span className={isVisited ? "" : 'home-menu-active'} onClick={this.handleChange(false)}>Sales</span>
-          </div>}
-          extra={<RangePicker
-            defaultValue={[moment('2019/01/01', dateFormat), moment('2019/06/01', dateFormat)]}
-            format={dateFormat}
-          />}
-        >
-          <Card
-            className="home-table-left"
-            title={isVisited ? 'Views trend' : 'Sales trend'}
-            bodyStyle={{padding: 0, height: 275}}
-            extra={<Icon type="reload"/>}
-          >
-            <Bar/>
-          </Card>
-
-          <Card title='Task' extra={<Icon type="reload"/>} className="home-table-right">
-            <Timeline>
-              <Timeline.Item color="green">New Version</Timeline.Item>
-              <Timeline.Item color="green">Complete website</Timeline.Item>
-              <Timeline.Item color="red">
-                <p>API</p>
-                <p>Functions</p>
-              </Timeline.Item>
-              <Timeline.Item>
-                <p>Login design</p>
-                <p>Authorization</p>
-                <p>Interfaces/UI</p>
-              </Timeline.Item>
-            </Timeline>
-          </Card>
-        </Card>
       </div>
     )
   }
